c8c24b869042645f2ab6dee2e27cd51f8a05de76
hyooojing <wingsbegin@naver.com>
2025-09-14 19:53:59 +0900

    Array Readme add

--- java/day2/src/ch2/ForEx1.java
+++ java/day2/src/ch2/ForEx1.java
@@ -1,5 +1,7 @@
 package ch2;
 
+import java.util.Scanner;
+
 /*
  	for 반복문	
  	정해진 조건만큼 반복
@@ -46,6 +48,25 @@ public class ForEx1 {
 			rslt += i;
 		}
 		System.out.println("합계: " + rslt);
+		
+		System.out.println("===========");
+		System.out.print("숫자를 입력해 주세요: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 1; i <= num; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("===========");
+		// 배열 Array: 여러 개의 값을 한 번에 저장한 변수, 인덱스를 가짐
+		// 한 번 크기를 정하면 변경할 수 없고 같은 자료형만 저장 가능
+		// 자료형[] 배열명 = { }; OR 자료형[] 배열명 = new 자료형[크기];
+		// 읽는 방식: 배열명[인덱스번호]
+		// .length: 배열이나 문자열의 길이, 크기, 갯수를 알려주는 속성
+		int[] nums = { 3, 5, 7, 9 };
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
 	}
 
 }
--- java/day2/src/ch2/Hw1.java
+++ java/day2/src/ch2/Hw1.java
@@ -1,11 +1,13 @@
 package ch2;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hw1 {
 
 	public static void main(String[] args) {
 
+		
 		// 1.치킨 주문 알림기
 		Scanner sc = new Scanner (System.in);
 		System.out.print("치킨을 주문해 주세요(마리): ");
@@ -27,7 +29,10 @@ public class Hw1 {
 		
 		// 3.입력 받은 수의 짝수만 출력하는 계산기
 		System.out.println("====================");
-		
+		System.out.print("숫자를 입력해 주세요: ");
+		int num = sc.nextInt();
+		if (num % 2 == 0)
+			System.out.println(num);
 		
 		// 4.두 수를 입력 받아 그 사이 숫자 모두 출력
 		System.out.println("====================");
@@ -65,6 +70,7 @@ public class Hw1 {
 			System.out.println("패스!");
 		
 		// 8.입력한 숫자 * 입력한 숫자 크기의 별 출력
+		System.out.println("====================");
 		System.out.print("숫자를 입력해 주세요: ");
 		int n4 = sc.nextInt();
 		for (int i = 0; i < n4; i++) {
@@ -74,7 +80,41 @@ public class Hw1 {
 			System.out.println();
 		}
 		
-		// 9.
+		// 9. 피자 몇 조각?
+		System.out.println("====================");
+		System.out.print("피자는 몇 조각? : ");
+		int pizza = sc.nextInt();
+		System.out.print("사람은 몇 명?: ");
+		int person = sc.nextInt();
+		
+		System.out.println("한 사람 당 " + (pizza / person) + "조각 가능!");
+		System.out.println("남은 조각은 " + (pizza % person) + "조각!");
+		
+
+		// 10. 주사위 결과
+		System.out.println("====================");
+		Random random = new Random();
+		int rslt1 = random.nextInt(6) + 1;
+		System.out.println("첫번째 주사위 값: " + rslt1);
+		int rslt2 = random.nextInt(6) + 1;
+		System.out.println("두번째 주사위 값: " + rslt2);
+		System.out.println("두 수의 합: " + (rslt1 + rslt2));
+		
+		
+		// 11.랜덤 숫자 맞추기
+		System.out.println("====================");
+		int secretNum = random.nextInt(50) + 1;
+		int n5;
+		
+		do {
+			System.out.print("숫자를 입력해 주세요: ");
+			n5 = sc.nextInt();
+			if (n5 > secretNum) 
+				System.out.println("DOWN!");
+			else if (n5 < secretNum)
+				System.out.println("UP!");
+		} while (n5 != secretNum);
+		System.out.println("정답입니다!");
 	}
 
 }
